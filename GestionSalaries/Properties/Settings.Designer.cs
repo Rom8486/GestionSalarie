b2889ddc0735eaab118e8803730bbab6d6b25e74
romain sire <romsire@yahoo.fr>
2021-03-10 09:07:50 +0100

    changement chemin du dossier

--- GestionSalaries/Properties/Settings.Designer.cs
+++ GestionSalaries/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GestionSalaraies.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,8 @@ namespace GestionSalaraies.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\CDA\\Desktop")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\CDA\\Desktop\\Git_test_gestion_Salarie\\clone_gestion\\SalariesDll.Salaries." +
+            "Xml")]
         public string AppData {
             get {
                 return ((string)(this["AppData"]));
